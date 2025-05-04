d4c5ad14fe8a4ffdc0a6014b422f5d04d7ba9722
warungrizkysatrio <warungrizkysatrio@gmail.com>
2025-05-05 04:24:46 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const animated = document.querySelectorAll('.header, .hero-overlay, .gallery, .about, .contact');
-  animated.forEach(el => el.style.animation = "fadeSlide 1s ease forwards");
-});
